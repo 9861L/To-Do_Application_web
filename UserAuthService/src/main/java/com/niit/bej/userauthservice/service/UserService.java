a1d6afa12f34fd6b0a816512d383457333b43b2c
P LALIT KUMAR REDDY <76241959+9861L@users.noreply.github.com>
2023-11-03 13:47:09 +0530

    Add files via upload

---
+++ UserAuthService/src/main/java/com/niit/bej/userauthservice/service/UserService.java
@@ -0,0 +1,11 @@
+package com.niit.bej.userauthservice.service;
+
+import com.niit.bej.userauthservice.domain.User;
+import com.niit.bej.userauthservice.exception.UserNotFoundException;
+import com.niit.bej.userauthservice.exception.UserAlreadyExistException;
+
+public interface UserService {
+    User registerUser(User user) throws UserAlreadyExistException;
+    User findByEmailAndPassword(String emailId , String password) throws UserNotFoundException;
+
+}
